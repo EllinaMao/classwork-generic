26122f5d04724d563962d06744bb5343c9730b47
Olga <xotaoshi@gmail.com>
2025-07-01 20:26:31 +0300

    //updated task3, temp

---
+++ EmployersList/Employers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployersList
+{    /*
+Задание 3
+Создайте приложение для учёта сотрудников. Необходимо
+хранить следующую информацию:
+ ФИО
+ Должность
+ Заработная плата
+ Рабочий email
+
+     */
+    internal struct Employers
+    {
+        public string FullName{ get; set; }
+        public string Position { get; set; }
+        public double Salary { get; set; }
+        public MailAddress WorkEmail { get; set; }
+
+        public Employers() {
+            FullName = "Unknown";
+            Position = "Unknown";
+            Salary = 0.0;
+            WorkEmail = new MailAddress("example@gmail.com");
+        }
+        public Employers(string fullName, string position = "Unknown", double salary = 0.0, string email = "example@gmail.com")
+        {
+            FullName = fullName;
+            Position = position;
+            Salary = salary;
+            WorkEmail = new MailAddress(email);
+        }
+    }
+}
---
+++ EmployersList/EmployersList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployersList
+{
+    /*
+Для хранения данных сотрудников используйте класс List<T>.
+Приложение должно предоставлять такую функциональность:
+Добавление сотрудников
+Удаление сотрудников
+Изменение информации о сотрудниках
+Поиск сотрудников по параметрам
+Сортировка сотрудников по параметрам*/
+    internal class EmployersList
+    {
+        List<Employers> employers;
+        public EmployersList()
+        {
+            employers = new List<Employers>();
+        }
+        public EmployersList(List<Employers> employers)
+        {
+            this.employers = employers ?? new List<Employers>();
+        }
+        public void AddEmployer(Employers employer)
+        {
+            employers.Add(employer);
+        }
+        public void RemoveEmployer(Employers employer)
+        {
+            employers.Remove(employer);
+        }
+        public void UpdateEmployer(int index, Employers employer)
+        {
+            try
+            {
+
+            if (index < 0 || index >= employers.Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), "Индекс вне списка сотрудников");
+            }
+            employers[index] = employer;
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        public List<Employers> Search
+
+
+    }
+}
